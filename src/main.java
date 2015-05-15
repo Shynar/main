cc9a69abbf35160486f9808663f31bf8fc08542a
Shynar <shinar_28@mail.ru>
2015-05-15 14:06:43 +0600

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Шынар on 14.05.2015.
+ */
+public class main {
+    public static void main(String[] args) {
+        String x = "Hello!";
+        System.out.println(x);
+    }
+}
